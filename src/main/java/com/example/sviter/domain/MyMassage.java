8ba6f14ea21a8faed2cbba96ed80b33ca3d54248
Edko <Edko@ua.fm>
2018-12-30 15:46:53 +0200

    7 commit

--- src/main/java/com/example/sviter/domain/MyMassage.java
+++ src/main/java/com/example/sviter/domain/MyMassage.java
@@ -2,10 +2,7 @@ package com.example.sviter.domain;
 
 import sun.plugin2.message.Message;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class MyMassage {
@@ -16,12 +13,24 @@ public class MyMassage {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
     public MyMassage() {
     }
 
     public MyMassage(String text, String tag) {
         this.text = text;
         this.tag = tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public Integer getId() {
