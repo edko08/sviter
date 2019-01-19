3c5925b6a4143375660ba66b39c4193f3f7ceeff
Ed <edko@ua.fm>
2019-01-19 12:39:45 +0200

    18 commit

--- src/main/java/com/example/sviter/config/MvcConfig.java
+++ src/main/java/com/example/sviter/config/MvcConfig.java
@@ -4,7 +4,6 @@ package com.example.sviter.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
